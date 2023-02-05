247a0c070027c1071a3acd7df64e95142d991e20
MomanyiPoet <momanyiturner@gmail.com>
2023-02-05 04:11:07 +0300

    updated iteration

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+include 'connection.php';
+?>
 
 <!DOCTYPE html>
 <html lang="en" itemscope itemtype="http://schema.org/WebPage">
@@ -50,7 +53,7 @@
           </li>
 
           <li class="nav-item dropdown dropdown-hover mx-2 ms-lg-6">
-            <a class="nav-link" href="login.php">
+            <a class="nav-link" href="admin/login.php">
               Login
             </a>
           </li>
@@ -81,102 +84,50 @@
     <!-- -------- START Features w/ pattern background & stats & rocket -------- -->
     <section class="pb-5 position-relative bg-gradient-dark mx-n3">
       <div class="container">
+
         <div class="row">
           <div class="col-md-8 text-start mb-5 mt-5">
             <h3 class="text-white z-index-1 position-relative">The Executive Team</h3>
             <p class="text-white opacity-8 mb-0">There’s nothing I really wanted to do in life that I wasn’t able to get good at. That’s my skill.</p>
           </div>
         </div>
+
         <div class="row">
+          <?php
+            $ret=mysqli_query($conn,"SELECT * from  tblportfolio");
+            $cnt=1;
+            while ($row=mysqli_fetch_array($ret)) {
+          ?>
           <div class="col-lg-6 col-12">
             <div class="card card-profile mt-4">
               <div class="row">
-                <div class="col-lg-4 col-md-6 col-12 mt-n5">
-                  <a href="javascript:;">
-                    <div class="p-3 pe-md-0">
-                      <img class="w-100 border-radius-md shadow-lg" src="assets/img/bruce-mars.jpg" alt="image">
-                    </div>
-                  </a>
-                </div>
-                <div class="col-lg-8 col-md-6 col-12 my-auto">
-                  <div class="card-body ps-lg-0">
-                    <h5 class="mb-0">Member</h5>
-                    <h6 class="text-info">UI Designer</h6>
-                    <p class="mb-0">Artist is a term applied to a person who engages in an activity deemed to be an art.</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-6 col-12">
-            <div class="card card-profile mt-lg-4 mt-5">
-              <div class="row">
-                <div class="col-lg-4 col-md-6 col-12 mt-n5">
-                  <a href="javascript:;">
-                    <div class="p-3 pe-md-0">
-                      <img class="w-100 border-radius-md shadow-lg" src="assets/img/bruce-mars.jpg" alt="image">
-                    </div>
-                  </a>
-                </div>
-                <div class="col-lg-8 col-md-6 col-12 my-auto">
-                  <div class="card-body ps-lg-0">
-                    <h5 class="mb-0">Member</h5>
-                    <h6 class="text-info">Data Analyst</h6>
-                    <p class="mb-0">Artist is a term applied to a person who engages in an activity deemed to be an art.</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
 
-        <div class="row mt-4">
-          <div class="col-lg-6 col-12">
-            <div class="card card-profile mt-4 z-index-2">
-              <div class="row">
                 <div class="col-lg-4 col-md-6 col-12 mt-n5">
                   <a href="javascript:;">
                     <div class="p-3 pe-md-0">
-                      <img class="w-100 border-radius-md shadow-lg" src="assets/img/self.jpg" alt="image">
+                      <img class="w-100 border-radius-md shadow-lg" src="admin/images/<?php echo $row['Image']?>" alt="image">
                     </div>
                   </a>
                 </div>
+                
                 <div class="col-lg-8 col-md-6 col-12 my-auto">
+
                   <div class="card-body ps-lg-0">
-                    <h5 class="mb-0">Momanyi Brian</h5>
-                    <h6 class="text-info">Front End Developer</h6>
-                    <p class="mb-0">IT Professional and Software Developer<br>
-                      <a href="author4.php" class="text-info text-sm icon-move-right">Read More
-                        <i class="fas fa-arrow-right text-xs ms-1"></i>
-                      </a>
-                    </p>
+                    <h5 class="mb-0"><?php  echo $row['lastName'];?> <?php  echo $row['firstName'];?></h5>
+                    <h6 class="text-info"><?php  echo $row['title'];?></h6>
+                    <p class="mb-0"><?php  echo $row['skills'];?></p>
                   </div>
+
                 </div>
               </div>
             </div>
           </div>
 
-          <div class="col-lg-6 col-12">
-            <div class="card card-profile mt-lg-4 mt-5 z-index-2">
-              <div class="row">
-                <div class="col-lg-4 col-md-6 col-12 mt-n5">
-                  <a href="javascript:;">
-                    <div class="p-3 pe-md-0">
-                      <img class="w-100 border-radius-md shadow-lg" src="assets/img/bruce-mars.jpg" alt="image">
-                    </div>
-                  </a>
-                </div>
-                <div class="col-lg-8 col-md-6 col-12 my-auto">
-                  <div class="card-body ps-lg-0">
-                    <h5 class="mb-0">Member</h5>
-                    <h6 class="text-info">JS Developer</h6>
-                    <p class="mb-0">Artist is a term applied to a person who engages in an activity deemed to be an art.</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <?php
+            $cnt=$cnt+1;
+          }?>
         </div>
+
       </div>
     </section>
   </div>
